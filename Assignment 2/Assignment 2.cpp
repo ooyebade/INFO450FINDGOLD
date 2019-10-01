c91432e761b0fd49e8267bed421be93214ff9efa
ooyebade <51129085+ooyebade@users.noreply.github.com>
2019-10-01 02:00:04 -0400

    Update Assignment 2.cpp

--- Assignment 2/Assignment 2.cpp
+++ Assignment 2/Assignment 2.cpp
@@ -52,10 +52,74 @@ int initializeArray(char guessArray[Rows][Columns])
 	return 0;
 }
 
-//
+// First iterate the array and place question marks in every position
+int initializeArray(char guessArray[Rows][Columns])
+{
+	int rows, columns;
+
+	remainingGuesses = 5;
+
+	for (rows = 0; rows < Rows; rows++)
+	{
+		for (columns = 0; columns < Columns; columns++)
+		{
+			guessArray[rows][columns] = '?'; 
+		}
+	}
+	return 0;
+}
+
+// Secondly print the array onto the console
 int displayArray(char guessArray[Rows][Columns])
 {
-	
+	int rows, columns;
+	cout << endl;
+	cout << "  "; // Adds 2 space to the top row
+	for (columns = 0; columns < Columns; columns++)
+	{
+		cout << setw(2) << columns + 1;  // add 1 since we start at 0
+	}
+	cout << endl;
+	cout << "-------------------------" << endl;
+	for (rows = 0; columns < Rows; rows++)
+	{
+		cout << " " << rows + 1 << "|"; // add 1 since we start at 0
+		for (columns = 0; columns < Columns; columns++)
+		{
+			cout << setw(2) << guessArray[Rows][Columns];  // displays the array so the spacing can be more readable
+		}
+	}
+	cout << "-------------------------" << endl;
 	return 0;
+}
+
+// Thirdly explain the rules
+int coutGameRules()
+{
+	cout << "**" << "**********************" << "**" << endl;
+	cout << "**" << "                      " << "**" << endl;
+	cout << "**" << "       Find Gold!     " << "**" << endl;
+	cout << "**" << "                      " << "**" << endl;
+	cout << "**" << "   You have 5 guesses " << "**" << endl;
+	cout << "**" << "                      " << "**" << endl;
+	cout << "**" << "     You have...      " << "**" << endl;
+	cout << "**" << "    5 pieces of gold  " << "**" << endl;
+	cout << "**" << "       and 1 bomb!    " << "**" << endl;
+	cout << "**" << "                      " << "**" << endl;
+	cout << "**" << "        Have Fun      " << "**" << endl;
+	cout << "**" << "           and        " << "**" << endl;
+	cout << "**" << "        Good Luck!    " << "**" << endl;
+	cout << "**" << "                      " << "**" << endl;
+	cout << "**" << "**********************" << "**" << endl;
+	cout << endl;
+	cout << endl;
+
+	return 0;
+}
+
+//
+int populateArray(char guessArray[Rows][Columns])
+{
+
 }
 
